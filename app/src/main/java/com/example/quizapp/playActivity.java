3047fa2ca04d92ff2f4f2c7cd0dcaa07e53ae2aa
Hovhanes <hovhannes2dallakyan@gmail.com>
2024-05-20 21:49:19 +0400

    project_8 with timer

--- app/src/main/java/com/example/quizapp/playActivity.java
+++ app/src/main/java/com/example/quizapp/playActivity.java
@@ -4,38 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class playActivity extends AppCompatActivity {
-    String[] question_list = {"What year was this car made? Ford Model T",
+    String[] question_list = {
+            "What year was this car made? Ford Model T",
             "What year was this car made? Chevrolet Corvette",
             "What year was this car made? Volkswagen Beetle",
             "What year was this car made? Porsche 911",
+            "What year was this car made? Ford Mustang",
+            "What year was this car made? Nissan GT-R",
+            "What year was this car made? BMW 3 Series",
+            "What year was this car made? Chevrolet Camaro",
+            "What year was this car made? Ferrari Dino",
+            "What year was this car made? Lamborghini Countach",
+            "What year was this car made? Dodge Challenger",
+            "What year was this car made?Mercedes-Benz S-Class",
+            "What year was this car made?Tesla Model S",
             "What year was this car made? Toyota Corolla",
             "What year was this car made? Honda Civic",
-            "What year was this car made? BMW 3 Series",
-            "What year was this car made? Ford Mustang",
-            "What year was this car made? Mercedes-Benz S-Class",
-            "What year was this car made? Jeep Wrangler",
-            "What year was this car made? Dodge Challenger"};
-    String[] choose_list = {"1908", "1911", "2000", "1823",
-            "2001", "1954", "1953", "1845",
-            "1990", "1938", "1998", "1801",
-            "1963", "1909", "1880", "2001",
-            "1914", "2011", "1699", "1966",
-            "1978", "1999", "1956", "1972",
-            "1890", "1999", "1975", "1888",
-            "1964", "1897", "2003", "1867",
-            "1858", "1789", "1899", "1972",
-            "1789", "1986", "1987", "1789",
-            "1699", "1966", "1869", "1970"
+            "What year was this car made? BMW M3",
+            "What year was this car made? Audi R8",
+            "What year was this car made? Lamborghini Aventador",
+            "What year was this car made? Ford GT",
+            "What year was this car made? Bugatti Veyron"
     };
-    String[] correct_list = {"1908", "1953", "1938", "1963", "1966", "1972", "1975", "1964", "1972", "1986", "1970"};
 
-    TextView cpt_question, text_question;
+    String[] choose_list = {
+            "1908", "1911", "2000", "1823",
+            "2001", "1954", "1953", "1845",
+            "1938", "1939", "1940", "1941",
+            "1963", "1964", "1965", "1966",
+            "1966", "1967", "1968", "1969",
+            "1972", "1973", "1974", "1975",
+            "1976", "1977", "1975", "1978",
+            "1965", "1966", "1967", "1964",
+            "1972", "1973", "1974", "1975",
+            "1987", "1986", "1988", "1989",
+            "1971", "1972", "1970", "1973",
+            "1985", "1854", "2001", "1972",
+            "2012", "2009", "1955", "1999",
+            "1966", "1970", "1985", "2014",
+            "1972", "1980", "1965", "1990",
+            "1995", "1986", "1978", "2000",
+            "1999", "2010", "2015", "2006",
+            "2011", "2005", "2018", "2000",
+            "2005", "1998", "2012", "2016",
+            "2000", "2010", "2005", "2015"
+    };
+
+    String[] correct_list = {
+            "1908", "1953", "1938", "1963", "1966",
+            "1972", "1975", "1964", "1972", "1986",
+            "1970", "1972", "2012", "1966", "1972",
+            "1986", "2006", "2011", "2005", "2005"
+    };
+
+    TextView cpt_question, text_question, timerText;
     Button btn_choose1, btn_choose2, btn_choose3, btn_choose4, btn_check;
 
     int currentQuestion = 0;
@@ -46,10 +75,14 @@ public class playActivity extends AppCompatActivity {
     Button btn_click;
     boolean[] answeredQuestions;
 
+    private static final long TIMER_DURATION = 4 * 60 * 1000; // 4 rope
+    CountDownTimer countDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
         cpt_question = findViewById(R.id.cpt_question);
         text_question = findViewById(R.id.text_question);
         btn_check = findViewById(R.id.btn_check);
@@ -57,12 +90,15 @@ public class playActivity extends AppCompatActivity {
         btn_choose2 = findViewById(R.id.btn_choose2);
         btn_choose3 = findViewById(R.id.btn_choose3);
         btn_choose4 = findViewById(R.id.btn_choose4);
+        timerText = findViewById(R.id.timer);
 
         answeredQuestions = new boolean[question_list.length];
 
         findViewById(R.id.image_back).setOnClickListener(
                 a -> finish()
         );
+
+        startTimer();
         remplirData();
 
         btn_check.setOnClickListener(view -> {
@@ -80,14 +116,11 @@ public class playActivity extends AppCompatActivity {
                     scorePlayer++;
                 }
 
-                // Mark the question as answered and disable the buttons
                 answeredQuestions[currentQuestion] = true;
                 disableButtons();
 
-                // Change button text to "Next"
                 btn_check.setText("Next");
             } else if (isAnswerChecked) {
-                // Move to the next question
                 if (currentQuestion != question_list.length - 1) {
                     currentQuestion++;
                     remplirData();
@@ -95,10 +128,7 @@ public class playActivity extends AppCompatActivity {
                     isAnswerChecked = false;
                     btn_check.setText("Check");
                 } else {
-                    Intent intent = new Intent(playActivity.this, ResulteActivity.class);
-                    intent.putExtra("Result", scorePlayer);
-                    startActivity(intent);
-                    finish();
+                    endQuiz();
                 }
             } else {
                 Toast.makeText(playActivity.this, "Please select an answer", Toast.LENGTH_LONG).show();
@@ -115,7 +145,6 @@ public class playActivity extends AppCompatActivity {
         btn_choose3.setText(choose_list[4 * currentQuestion + 2]);
         btn_choose4.setText(choose_list[4 * currentQuestion + 3]);
 
-        // Reset button backgrounds and states
         resetButtons();
     }
 
@@ -140,7 +169,6 @@ public class playActivity extends AppCompatActivity {
         btn_choose3.setBackgroundResource(R.drawable.background_btn_default);
         btn_choose4.setBackgroundResource(R.drawable.background_btn_default);
 
-        // Disable buttons if the question is already answered
         if (answeredQuestions[currentQuestion]) {
             btn_choose1.setEnabled(false);
             btn_choose2.setEnabled(false);
@@ -171,5 +199,29 @@ public class playActivity extends AppCompatActivity {
         } else if (btn_choose4.getText().toString().equals(correct_list[currentQuestion])) {
             btn_choose4.setBackgroundResource(R.drawable.background_btn_correct1);
         }
+    }
+
+    void startTimer() {
+        countDownTimer = new CountDownTimer(TIMER_DURATION, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                timerText.setText(String.format("%02d:%02d", minutes, seconds));
+            }
+
+            @Override
+            public void onFinish() {
+                endQuiz();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    void endQuiz() {
+        Intent intent = new Intent(playActivity.this, ResulteActivity.class);
+        intent.putExtra("Result", scorePlayer);
+        startActivity(intent);
+        finish();
     }
 }
